97c165f0ee770a423a46a8dadfb091d3ffefae73
tom tait <thomas.t.ca@outlook.com>
2024-05-23 15:58:04 -0300

    webapp

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
-const axios = require('axios');
+document.getElementById('btn').addEventListener('click', translate)
 
 
-async function translate(input)
+async function translate() 
 {
   const encodedParams = new URLSearchParams();
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  const input = document.getElementById("input").value;
+  console.log('translating: ' , input)
   encodedParams.set('q', input);
-  encodedParams.set('target', 'is');
-  encodedParams.set('source', 'en');
+  encodedParams.set('source', from);
+  encodedParams.set('target', to);
 
-  const options = 
-  {
+  const options = {
     method: 'POST',
     url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
-      'Accept-Encoding': 'application/gzip',
       'X-RapidAPI-Key': '1c4a1d048bmsh51aa5b380f1de7dp123a41jsn6f9b86f11bc7',
       'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
     },
     data: encodedParams,
   };
 
-  try 
-  {
+  try {
     const response = await axios.request(options);
-    console.log("translated text:\n" + response.data.data.translations[0].translatedText)
-  } 
-  catch (error) 
-  {
+    const translatedText =  response.data.data.translations[0].translatedText
+    console.log("translated text:\n" + translatedText);
+    document.getElementById("output").textContent = decode(translatedText);
+  } catch (error) {
     console.error(error);
+    document.getElementById("output").textContent = "Error: " + error;
   }
 }
 
-
-module.exports = { translate }
+function decode(text) {
+  const textArea = document.createElement('textarea');
+  textArea.innerHTML = text;
+  return textArea.value;
+}
